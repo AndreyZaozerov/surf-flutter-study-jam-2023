da1aa3fbc4f10861c7690bea1550de20d2bc86c3
Andrey Zaozerov <zaozerov@mail.ru>
2023-04-09 10:53:58 +0300

    add main page

--- lib/features/ticket_storage/ticket_storage_page.dart
+++ lib/features/ticket_storage/ticket_storage_page.dart
@@ -4,8 +4,57 @@ import 'package:flutter/material.dart';
 class TicketStoragePage extends StatelessWidget {
   const TicketStoragePage({Key? key}) : super(key: key);
 
+
+  Widget clearBody(){
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("Здесь пока ни чего нет"),
+        ],
+      ),
+    );
+  }
+
+
+  Widget addBtn(){
+    return SizedBox(
+      width: 120,
+      child: FloatingActionButton(
+        onPressed: () { },
+        child: const Text("Добавить"),
+      ),
+    );
+  }
+
+  Widget downloadBtn(){
+    return SizedBox(
+      width: 120,
+      child: FloatingActionButton(
+        onPressed: () { },
+        child: const Text("Загрузить всё"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const FlutterLogo();
+    // return const FlutterLogo();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Хранение билетов"),
+      ),
+      body: clearBody(),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          addBtn(),
+          // const SizedBox(
+          //   width: 10,
+          // ),
+          // downloadBtn(),
+        ],
+      ),
+    );
   }
 }
